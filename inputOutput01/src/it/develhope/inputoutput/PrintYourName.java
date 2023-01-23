5091056316d945a6f0630de8f5b2b4ec1ab799ac
Federico Urbani <urbanifederico5@gmail.com>
2023-01-23 13:00:19 +0100

    es 1 input

---
+++ inputOutput01/src/it/develhope/inputoutput/PrintYourName.java
@@ -0,0 +1,9 @@
+package it.develhope.inputoutput;
+
+import java.util.Scanner;
+
+public class PrintYourName {
+    public static void main(String []args){
+        System.out.println(new String("federico"));
+    }
+}
